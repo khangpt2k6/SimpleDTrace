6efe7044c5d1ed92d9363705dc8b595564a8c92f
khangpt2k6 <2006tuankhang@gmail.com>
2025-11-09 16:11:15 -0600

    update dtrace

--- dtrace_scripts/cpu_usage.d
+++ dtrace_scripts/cpu_usage.d
@@ -1,24 +1,24 @@
 #!/usr/bin/bpftrace
 
 tracepoint:sched:sched_switch
-/next_comm == "python" || next_comm == "python3"/
+/args->next_comm == "python" || args->next_comm == "python3"/
 {
     printf("[%s] %s (PID:%d) on CPU\n",
-           strftime("%H:%M:%S", nsecs / 1e9), next_comm, next_pid);
+           strftime("%H:%M:%S", nsecs / 1e9), args->next_comm, args->next_pid);
 }
 
 tracepoint:sched:sched_switch
-/prev_comm == "python" || prev_comm == "python3"/
+/args->prev_comm == "python" || args->prev_comm == "python3"/
 {
     printf("[%s] %s (PID:%d) off CPU\n",
-           strftime("%H:%M:%S", nsecs / 1e9), prev_comm, prev_pid);
+           strftime("%H:%M:%S", nsecs / 1e9), args->prev_comm, args->prev_pid);
 }
 
 tracepoint:sched:sched_wakeup
-/comm == "python" || comm == "python3"/
+/args->comm == "python" || args->comm == "python3"/
 {
     printf("[%s] %s (PID:%d) woken up\n",
-           strftime("%H:%M:%S", nsecs / 1e9), comm, pid);
+           strftime("%H:%M:%S", nsecs / 1e9), args->comm, args->pid);
 }
 
 END
